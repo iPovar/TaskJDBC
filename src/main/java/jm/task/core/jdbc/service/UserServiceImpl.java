7fbc43c2bcbefe75b1268ae5b69c58482e3b6bf9
iPovar <i-povar4enkov@mail.ru>
2020-12-11 20:44:40 +0300

    1.1.3 JDBC

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -12,7 +12,7 @@ public class UserServiceImpl implements UserService {
     private final UserDao userDao;
 
     public UserServiceImpl() {
-        userDao = new UserDaoHibernateImpl();
+        userDao = new UserDaoJDBCImpl();
     }
 
     public void createUsersTable() {
